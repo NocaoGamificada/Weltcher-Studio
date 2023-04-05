53f1b892d0d2b7979a8a8ff2c6277bfd4612b6c7
NocaoGamificada <116852859+NocaoGamificada@users.noreply.github.com>
2023-04-04 23:30:10 -0300

    update trabalhos

--- site.js
+++ site.js
@@ -6,6 +6,8 @@ const btnEquipe = document.getElementById("btnEquipe");
 btnEquipe.onclick = scrollToEquipe;
 const btnTec = document.getElementById("btnTec");
 btnTec.onclick = scrollToTecnologias;
+const btnTrabalhos = document.getElementById('btnTrabalhos');
+btnTrabalhos.onclick = scrollToTrabalhos;
 
 function scrollToEquipe()
 {
@@ -28,6 +30,12 @@ function scrollToSobre() {
 
 function scrollToGames() {
     const div = document.getElementById("jogos"); // substitua "minhaDiv" pelo ID da sua div
+    const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
+    window.scrollTo({top: y, behavior: 'smooth'});
+}
+
+function scrollToTrabalhos() {
+    const div = document.getElementById("trabalhos"); // substitua "minhaDiv" pelo ID da sua div
     const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
     window.scrollTo({top: y, behavior: 'smooth'});
 }
