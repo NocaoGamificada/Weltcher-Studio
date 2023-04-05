5013593297a9e9bde1c076dc4511e9a71fd04e33
NocaoGamificada <116852859+NocaoGamificada@users.noreply.github.com>
2023-04-04 21:19:27 -0300

    update

--- site.js
+++ site.js
@@ -10,24 +10,24 @@ btnTec.onclick = scrollToTecnologias;
 function scrollToEquipe()
 {
     const div = document.getElementById("equipe"); // substitua "minhaDiv" pelo ID da sua div
-    const y = div.getBoundingClientRect().top + window.pageYOffset;
+    const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
     window.scrollTo({top: y, behavior: 'smooth'});
 }
 
 function scrollToTecnologias() {
     const div = document.getElementById("tecnologias"); // substitua "minhaDiv" pelo ID da sua div
-    const y = div.getBoundingClientRect().top + window.pageYOffset;
+    const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
     window.scrollTo({top: y, behavior: 'smooth'});
 }
 
 function scrollToSobre() {
     const div = document.getElementById("sobre"); // substitua "minhaDiv" pelo ID da sua div
-    const y = div.getBoundingClientRect().top + window.pageYOffset;
+    const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
     window.scrollTo({top: y, behavior: 'smooth'});
 }
 
 function scrollToGames() {
     const div = document.getElementById("jogos"); // substitua "minhaDiv" pelo ID da sua div
-    const y = div.getBoundingClientRect().top + window.pageYOffset;
+    const y = div.getBoundingClientRect().top + window.pageYOffset - 16;
     window.scrollTo({top: y, behavior: 'smooth'});
 }
